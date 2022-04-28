da4f1c7822f5653b630fb9cafca196094a3b3f5b
Faye Lawson <70175491+falawson@users.noreply.github.com>
2022-04-28 12:33:17 -0700

    Update cookieTester.js

--- cookieTester.js
+++ cookieTester.js
@@ -70,14 +70,19 @@ function getCookie(name) {
 
 function checkCookie() {
     let user = getCookie("username");
-    if (user != "") {
-      alert("Welcome again " + user);
-    } else {
-      user = prompt("Please enter your name:", "");
-      if (user != "" && user != null) {
+    // if (user != "") {
+    //   alert("Welcome again " + user);
+    // } else {
+    //   user = prompt("Please enter your name:", "");
+    //   if (user != "" && user != null) {
+    //     setCookie("username", user, 365);
+    //     alert("new cookie added: " + getCookie("username"));
+    //   }
+    // }
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
         setCookie("username", user, 365);
         alert("new cookie added: " + getCookie("username"));
-      }
     }
 }
 
